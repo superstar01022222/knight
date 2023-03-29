5bd5da8287c8f95c5eb7c8e852c12b285ffe151b
Dat <you@example.com>
2023-03-29 13:42:19 +0700

    update

--- src/knight.cpp
+++ src/knight.cpp
@@ -1,21 +1,26 @@
 #include "knight.h"
 
-bool is_prime(int n) // checked
+int r,t,k;   //temp
+
+bool is_prime(int n)
 {
 	if (n == 0 || n == 1)
 		return false;
-	if (n == 2)
+	else if (n == 2)
 		return true;
-	for (unsigned int i = 2; i < n; i++)
+	else {
+	for (int i = 2; i < n; i++)
 	{
 		if (n % i == 0)
 			return false;
 	}
 	return true;
 }
+}
 
-int to_next_prime(int n) // checked
+int to_next_prime(int &n)
 {
+	//cout<<n<<endl;
 	n++;
 	while (!is_prime(n))
 	{
@@ -24,7 +29,7 @@ int to_next_prime(int n) // checked
 	return n;
 }
 
-int fibo(int n) // checked
+int fibo(int n)
 {
 	int fibo[50];
 	fibo[0] = 0;
@@ -37,7 +42,7 @@ int fibo(int n) // checked
 	return fibo[n];
 }
 
-bool is_fibo(int n) // checked
+bool is_fibo(int n)
 {
 	int i = 0;
 	while (fibo(i) < n)
@@ -50,7 +55,7 @@ bool is_fibo(int n) // checked
 		return false;
 }
 
-int maxHP, HP, level, remedy, maidenkiss, phoenixdown;
+int maxHP;
 bool is_tiny = false;
 int countdown_tiny = 0;
 bool is_frog = false;
@@ -61,11 +66,17 @@ bool merlin_yet = false;
 bool asclepius_yet = false;
 bool is_arthur = false;
 bool is_lancelot = false;
+int tempPhoenixdown = 0; 
 
-void tiny()
+int tiny(int &remedy,int &HP,int &phoenixdown)
 {
 	if (is_tiny)
 	{
+		if (tempPhoenixdown>phoenixdown){
+			is_tiny = false;
+			countdown_tiny = 0;
+			return 1;
+		}
 		if (remedy > 0)
 		{
 			HP = min(maxHP, HP * 5);
@@ -82,12 +93,19 @@ void tiny()
 		else
 			countdown_tiny--;
 	}
+	return 1;
 }
 
-void frog()
+int frog(int &maidenkiss,int &level,int &phoenixdown)
 {
 	if (is_frog)
 	{
+		if (tempPhoenixdown>phoenixdown){
+			is_frog = false;
+			countdown_frog = 0;
+			level = prelevel;
+			return 1;
+		}
 		if (maidenkiss)
 		{
 			is_frog = false;
@@ -103,19 +121,16 @@ void frog()
 		else
 			countdown_frog--;
 	}
+	return 1;
 }
 
-void is_continue()
+int is_continue(int &HP,int &phoenixdown,int &rescue)
 {
 	if (HP <= 0 && phoenixdown)
 	{
 		HP = maxHP;
 		phoenixdown--;
 		rescue = -1;
-		is_frog = false;
-		is_tiny = false;
-		countdown_frog = 0;
-		countdown_tiny = 0;
 	}
 	else if (HP <= 0 && phoenixdown == 0)
 	{
@@ -123,136 +138,152 @@ void is_continue()
 	}
 	else
 		rescue = -1;
+	return 1;
 }
 
-void event1(int order)
+
+
+void event1(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order ;
 	if (level > levelO || is_arthur || is_lancelot)
 		level = min(10, level + 1);
 	else if (level < levelO)
 		HP -= levelO * 10;
-
-	tiny();
-	frog();
-	is_continue();
+	k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+	
 }
 
-void event2(int order)
+void event2(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order;
 	if (level > levelO || is_arthur || is_lancelot)
 		level = min(10, level + 1);
 	else if (level < levelO)
 		HP -= levelO * 15;
-	tiny();
-	frog();
-	is_continue();
+	k = is_continue(HP,phoenixdown,rescue);	
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+
+	
 }
 
-void event3(int order)
+void event3(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order;
 	if (level > levelO || is_arthur || is_lancelot)
 		level = min(10, level + 1);
 	else if (level < levelO)
 		HP -= levelO * 45;
-
-	tiny();
-	frog();
-	is_continue();
+k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+	
 }
-void event4(int order)
+void event4(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order;
 	if (level > levelO || is_arthur || is_lancelot)
 		level = min(10, level + 1);
 	else if (level < levelO)
 		HP -= levelO * 75;
-
-	tiny();
-	frog();
-	is_continue();
+k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+	
 }
-void event5(int order)
+void event5(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order;
 	if (level > levelO || is_arthur || is_lancelot)
 		level = min(10, level + 1);
 	else if (level < levelO)
 		HP -= levelO * 95;
-
-	tiny();
-	frog();
-	is_continue();
+k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+	
 }
-void event6(int order)
+void event6(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	if (is_frog || is_tiny)
 	{
-		tiny();
-		frog();
-		is_continue();
+		k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+	
 		return;
 	}
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order ;
 	if (level > levelO || is_arthur || is_lancelot)
 		level = min(10, level + 2);
 	else if (level < levelO)
 	{
-		is_tiny = true;
-		countdown_tiny = 3;
-		HP = max(1, HP / 5);
+		if(remedy>0){
+			remedy--;
+		}
+		else{
+			is_tiny = true;
+			countdown_tiny = 3;
+			HP = max(1, HP / 5);
+		}
 	}
-
-	tiny();
-	frog();
-	is_continue();
+k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);
+	
 }
 
-void event7(int order)
+void event7(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	if (is_frog || is_tiny)
-	{
-		tiny();
-		frog();
-		is_continue();
+	{	
+		k = is_continue(HP,phoenixdown,rescue);
+		r = tiny(remedy,HP,phoenixdown);
+		t = frog(maidenkiss,level,phoenixdown);
 		return;
 	}
-	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order % 10;
-	if (level > levelO)
+	int levelO = order > 6 ? (order % 10 > 5 ? order % 10 : 5) : order;
+	if (level > levelO || is_arthur || is_lancelot)
 	{
 		level = min(10, level + 2);
 	}
-	else if (level < levelO || is_arthur || is_lancelot)
-	{
-		is_frog = true;
-		countdown_frog = 3;
-		prelevel = level;
-		level = 1;
+	else if (level < levelO)
+	{	
+		if(maidenkiss>0){
+			maidenkiss--;
+		}
+		else{
+			is_frog = true;
+			countdown_frog = 3;
+			prelevel = level;
+			level = 1;
+		}
 	}
 
-	tiny();
-	frog();
-	is_continue();
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
 }
 
-void event11()
+void event11(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	int n1 = ((level + phoenixdown) % 5 + 1) * 3;
 	int s1 = 0;
-	for (int i = 99; i >= 1 && n1 > 0; i -= 2)
+	for (int i = 99; i >= 99 - (n1-1)*2  ; i -= 2)
 	{
 		s1 += i;
-		n1--;
 	}
 	HP += (s1 % 100);
-	HP = min(HP, to_next_prime(HP));
-	tiny();
-	frog();
-	is_continue();
+	to_next_prime(HP);
+	HP = min(maxHP,HP);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
 }
-void event12()
+void event12(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	if (HP > 1)
 	{
@@ -261,10 +292,10 @@ void event12()
 		{
 			HP--;
 		}
-	}
-	tiny();
-	frog();
-	is_continue();
+	}k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	
 }
 // declare array for number sequence in MushhostPack
 int a[100], aa[100];
@@ -272,10 +303,11 @@ int a[100], aa[100];
 // 4 type mushroom
 void mushtype1(int &maxi, int &mini, int length)
 {
-	for (int i = 0; i < length; i++)
+	int min = 2e9;
+	int max = -2e9;
+	for (int i = length-1; i >=0; i--)
 	{
-		int min = 2e9;
-		int max = -2e9;
+		
 		if (a[i] > max)
 		{
 			max = a[i];
@@ -325,13 +357,25 @@ void mushtype3(int &maxi2, int &mini2, int length)
 	}
 }
 
-void mushtype4(int &max2_3x, int &max2_3i)
+void mushtype4(int &max2_3x, int &max2_3i, int length)
 {
 
 	max2_3x = -5;
 	max2_3i = -7;
 	int mx = max(max(aa[0], aa[1]), aa[2]);
 	int mn = min(min(aa[0], aa[1]), aa[2]);
+	
+	if (length==2){
+		if (mx==mn)
+			return;
+		else{
+			max2_3x=mn;
+			if(mn==aa[1])
+				max2_3i = 1;
+			else
+				max2_3i = 0;
+		}
+	}
 	for (int i = 0; i < 3; i++)
 	{
 		if (aa[i] != mx && aa[i] != mn)
@@ -341,7 +385,7 @@ void mushtype4(int &max2_3x, int &max2_3i)
 		}
 	}
 }
-void event13(int num, string pack1)
+void event13(int num, string pack1,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	// read file mush ghost
 	string ss = to_string(num);
@@ -376,11 +420,11 @@ void event13(int num, string pack1)
 			aa[i] = a[i];
 		}
 
-		aa[i] = (17 * a[i] + 9) % 257;
+		aa[i] = (17 * aa[i] + 9) % 257;
 	}
 
 	int m, n;
-	for (int i = 2; i < ss.length() - 1; i++)
+	for (auto i = 2; i < (int)ss.length() ; i++)
 	{
 		if (ss[i] == '1')
 			mushtype1(m, n, length);
@@ -389,76 +433,91 @@ void event13(int num, string pack1)
 		else if (ss[i] == '3')
 			mushtype3(m, n, length);
 		else if (ss[i] == '4')
-			mushtype4(m, n);
+			mushtype4(m, n, length);
 		HP = min(maxHP, HP - (m + n));
-		is_continue();
+k = is_continue(HP,phoenixdown,rescue);
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	
+	//cout<<ss[i]<<" "<<HP<<endl;
 	}
 }
 
-void event15()
+void event15(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	remedy = min(remedy + 1, 99);
-	tiny();
-	frog();
-	is_continue();
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
 }
-void event16()
+void event16(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	maidenkiss = min(maidenkiss + 1, 99);
-	tiny();
-	frog();
-	is_continue();
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
 }
-void event17()
+void event17(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	phoenixdown = min(phoenixdown + 1, 99);
-	tiny();
-	frog();
-	is_continue();
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
 }
 
-void event19(string pack2)
+void event19(string pack2,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
+	
 	ifstream file;
 	file.open(pack2);
 	int array[100][100];
 	int x, y;
 	file >> x >> y;
+
 	for (int i = 0; i < x; ++i)
 	{
-		for (int j; i < y; ++j)
+		for (int j = 0; j < y; ++j)
 		{
 			file >> array[i][j];
 		}
 	}
 	file.close();
+	//cout<<"hellơ"<<endl;
 	for (int i = 0; i < x; ++i)
 	{
 		int take_line = 0;
-		for (int j = 0; i < y && take_line < 3; ++i)
+		for (int j = 0; j < y; ++j)
 		{
-			if (array[i][j] == 15)
+			if (take_line == 3)
+				break;
+			if (array[i][j] == 16)
 			{
-				remedy++;
+				if (remedy<99)
+					remedy++;
 				take_line++;
-				if (array[i][j] == 16)
-				{
+			}
+			if (array[i][j] == 17)
+			{
+				if (maidenkiss<99)
 					maidenkiss++;
-					take_line++;
-				}
-				if (array[i][j] == 17)
-				{
+				take_line++;
+			}
+			if (array[i][j] == 18)
+			{
+				if (phoenixdown<99)
 					phoenixdown++;
-					take_line++;
-				}
+				take_line++;
 			}
 		}
 	}
+	
 
-	tiny();
-	frog();
-	is_continue();
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
+
 	asclepius_yet = true;
+	//cout<<"hello"<<endl;
 }
 
 // check a string in the string
@@ -470,15 +529,17 @@ bool check_in_string(string s, string check)
 		return false;
 }
 
-void event18(string pack3)
+void event18(string pack3,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 { // merlin
 	const string s1 = "Merlin";
 	const string s2 = "merlin";
+	string name;
 	ifstream file;
 	file.open(pack3);
 	int n;
 	string name_thing[100];
 	file >> n;
+	getline(file,name);
 	for (int i = 0; i < n; ++i)
 	{
 		getline(file, name_thing[i]);
@@ -486,9 +547,9 @@ void event18(string pack3)
 	file.close();
 
 	int HP_gain = 0;
-	for (int i; i < n; ++i)
+	for (int i=0; i < n; ++i)
 	{
-		string name = name_thing[i];
+		name = name_thing[i];
 		if (check_in_string(name, s1) || check_in_string(name, s2))
 			HP_gain += 3;
 		else if ((check_in_string(name, "m") || check_in_string(name, "M")) && (check_in_string(name, "e") || check_in_string(name, "E")) && (check_in_string(name, "r") || check_in_string(name, "R")) && (check_in_string(name, "l") || check_in_string(name, "L")) && (check_in_string(name, "i") || check_in_string(name, "I")) && (check_in_string(name, "n") || check_in_string(name, "N")))
@@ -496,20 +557,22 @@ void event18(string pack3)
 	}
 	HP = min(maxHP, HP + HP_gain);
 	merlin_yet = true;
-	tiny();
-	frog();
-	is_continue();
+	r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
 }
 
-void event99()
+
+void event99(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue)
 {
 	if (level == 10 || is_arthur || (is_lancelot && level >= 8))
 	{
 		level = 10;
-		tiny();
-		frog();
-		is_continue();
-	}
+		r = tiny(remedy,HP,phoenixdown);
+	t = frog(maidenkiss,level,phoenixdown);;
+	k = is_continue(HP,phoenixdown,rescue);
+}
+	
 	else
 		rescue = 0;
 }
@@ -524,129 +587,148 @@ void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int
 		 << ", rescue=" << rescue << endl;
 }
 // function read data
-void read_input(string a, string l1, string l2, string l3)
-{
+
+
+// read file name in line 3
+
+
+void adventureToKoopa(string file_input, int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
+{	
+	string l1, l2, l3;
+	
+	rescue=-1;
+	HP=0;
+	level=0;
+	remedy=0;
+	maidenkiss=0;
+	phoenixdown=0;
+
 	ifstream file;
-	file.open(a);
+	file.open(file_input,ios::in);
+	file >> HP >> level >> remedy >> maidenkiss >> phoenixdown;
 	getline(file, l1);
 	getline(file, l2);
 	getline(file, l3);
 	file.close();
-}
 
-// read file name in line 3
-void read_pack(string pack1, string pack2, string pack3, string line3)
-{
-	stringstream file(line3);
-	getline(file, pack1, ',');
-	getline(file, pack2, ',');
-	getline(file, pack3, ',');
-}
+	//display(HP, level, remedy, maidenkiss, phoenixdown, rescue);
 
-void adventureToKoopa(string file_input, int &HP, int &level, int &remedy, int &maidenkiss, int &phoenixdown, int &rescue)
-{
-	string l1, l2, l3;
-	read_input(file_input, l1, l2, l3);
-	stringstream info(l1);
-	info >> HP >> level >> remedy >> maidenkiss >> phoenixdown;
 	maxHP = HP;
 	if (maxHP == 999)
 		is_arthur = true;
-
+	//display(HP, level, remedy, maidenkiss, phoenixdown, rescue);
 	if (is_prime(maxHP))
 		is_lancelot = true;
+
+
 	string pack1, pack2, pack3;
-	read_pack(pack1, pack2, pack3, l3);
+	stringstream fil(l3);
+	getline(fil, pack1, ',');
+	getline(fil, pack2, ',');
+	getline(fil, pack3, ',');
 
+	
 	int order = 1;
-	long int num;
-	l2 += " -1";
+	long int num, numTest;
+	l2 += "120";
+	//cout<<l2<<endl;
+	auto l5 = l2;
 	stringstream event(l2);
-
-	while (event >> num && rescue == -1)
+	stringstream eventk(l5);
+	//cout<<"hello"<<endl;	
+	event>>num;
+	eventk>>numTest;
+	while (rescue == -1)
 	{
-		if (num == 1 && rescue == -1)
+		//cout<<num<<endl;
+		eventk>>numTest;
+		tempPhoenixdown=phoenixdown;
+		if (num == 1)
 		{
-			event1(order);
+			event1(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 2 && rescue == -1)
+		if (num == 2)
 		{
-			event2(order);
+			event2(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 3 && rescue == -1)
+		if (num == 3)
 		{
-			event3(order);
+			event3(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 4 && rescue == -1)
+		if (num == 4)
 		{
-			event4(order);
+			event4(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 5 && rescue == -1)
+		if (num == 5)
 		{
-			event5(order);
+			event5(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 6 && rescue == -1)
+		if (num == 6)
 		{
-			event6(order);
+			event6(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 7 && rescue == -1)
+		if (num == 7)
 		{
-			event7(order);
+			event7(order,HP,level, remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 11 && rescue == -1)
+		if (num == 11)
 		{
-			event11();
+			event11(HP,level,remedy,maidenkiss,phoenixdown,rescue);
 		}
-		if (num == 12 && rescue == -1)
+		if (num == 12)
 		{
-			event12();
-		}
-
-		if (num == 15 && rescue == -1)
-		{
-			event15();
-		}
-		if (num == 16 && rescue == -1)
-		{
-			event16();
-		}
-		if (num == 17 && rescue == -1)
-		{
-			event17();
-		}
-		if (num == 18 && !merlin_yet && rescue == -1)
-		{
-			event18(pack3);
-		}
-		if (num == 19 && !asclepius_yet && rescue == -1)
-		{
-			event19(pack2);
+			event12(HP,level,remedy,maidenkiss,phoenixdown,rescue);
 		}
 
-		if (num == 0 && rescue == -1)
+		if (num == 15)
+		{
+			event15(HP,level,remedy,maidenkiss,phoenixdown,rescue);
+		}
+		if (num == 16)
+		{
+			event16(HP,level,remedy,maidenkiss,phoenixdown,rescue);
+		}
+		if (num == 17)
+		{
+			event17(HP,level,remedy,maidenkiss,phoenixdown,rescue);
+		}
+		if (num == 18 && !merlin_yet)
+		{
+			event18(pack3,HP,level,remedy,maidenkiss,phoenixdown,rescue);
+		}
+		if (num == 19 && !asclepius_yet)
+		{
+			event19(pack2,HP,level,remedy,maidenkiss,phoenixdown,rescue);
+		}
+
+		if (num == 0)
 		{
 			rescue = 1;
 		}
 		if (num == 99)
 		{
-			event99();
+			event99(HP,level,remedy,maidenkiss,phoenixdown,rescue);
 		}
-
-		if (num > 130 && rescue == -1)
+	
+		if (num > 130)
 		{
-			event13(num, pack1);
+			event13(num, pack1,HP,level,remedy,maidenkiss,phoenixdown,rescue);
 		}
-
-		if (rescue == 1 || rescue == 0)
+		if (numTest == 120)
 		{
+			rescue = 1;
 			display(HP, level, remedy, maidenkiss, phoenixdown, rescue);
 			break;
 		}
-
+		if (rescue == 1 || rescue == 0)
+		{
+			display(HP, level, remedy, maidenkiss, phoenixdown, rescue);
+		}
 		else
 		{
 			display(HP, level, remedy, maidenkiss, phoenixdown, rescue);
 			order++;
 		}
+		event>>num;
 	}
 }
--- src/knight.h
+++ src/knight.h
@@ -5,5 +5,35 @@
 
 void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
+int tiny(int &remedy,int &HP,int &phoenixdown);
+int frog(int &maidenkiss,int &level,int &phoenixdown);
+int is_continue(int &HP,int &phoenixdown,int &rescue);
+void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
+void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
+bool is_prime(int n);
+int to_next_prime(int &n);
+int fibo(int n);
+bool is_fibo(int n);
+void event1( int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event2(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event3( int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event4( int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event5(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event6(int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event7( int order,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event11(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event12(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void mushtype1(int &maxi, int &mini, int length);
+void mushtype2(int &mtx, int &mti, int length);
+void mushtype3(int &maxi2, int &mini2, int length);
+void mushtype4(int &max2_3x, int &max2_3i,int length);
+void event13(int num, string pack1,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event15(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event16(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event17(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event19(string pack2,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+bool check_in_string(string s, string check);
+void event18(string pack3,int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
+void event99(int &HP,int &level, int &remedy,int &maidenkiss,int &phoenixdown,int &rescue);
 
 #endif // __KNIGHT_H__
